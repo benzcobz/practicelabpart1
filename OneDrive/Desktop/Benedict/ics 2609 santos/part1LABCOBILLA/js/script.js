4c4a6bdc9cdb63c7f80009d9f3fcc4d10738f53a
Benedict Cobilla <b_cobilla@yahoo.com>
2021-08-30 18:27:17 +0800

    cobs added

---
+++ OneDrive/Desktop/Benedict/ics 2609 santos/part1LABCOBILLA/js/script.js
@@ -0,0 +1,6 @@
+function topFunction() { // When the user clicks on the button, scroll to the top of the document
+    document.documentElement.scrollTop = 0; // For Chrome browser 
+  } 
+  function msg() {
+    alert("Added to Cart");
+  }
